fa26c3e8c5c063a14065a0d3b4edf8f787ec9688
Sarah Pickrahn <spickrahn@gmail.com>
2025-03-25 09:27:09 -0700

    remove sad attempt at setting up msw

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,1 @@
-import { server } from "./mocks/server";
 import "@testing-library/jest-dom";
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
