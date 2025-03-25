221f37666851d9262d4249cccd3cec1b7fdcf927
Sarah Pickrahn <spickrahn@gmail.com>
2025-03-25 08:14:40 -0700

    add disabled styles to button

--- src/app/components/designSystem/Button.tsx
+++ src/app/components/designSystem/Button.tsx
@@ -12,10 +12,10 @@ export const Button = ({
   return (
     <button
       className={clsx(
-        "w-full sm:w-fit text-md font-bold py-1 px-3 drop-shadow-md rounded-sm cursor-pointer focus:outline-yellow-500",
+        "w-full sm:w-fit text-md font-bold py-1 px-3 drop-shadow-md rounded-sm cursor-pointer focus:outline-yellow-500 disabled:outline-black/80 disabled:cursor-not-allowed",
         variant === "primary"
-          ? "bg-foreground text-background outline-2 outline-black hover:bg-gray-800"
-          : "bg-background text-foreground outline-2 outline-dark hover:bg-gray-200"
+          ? "bg-foreground text-background outline-2 outline-black hover:bg-gray-800 disabled:bg-gray-700"
+          : "bg-background text-foreground outline-2 outline-dark hover:bg-gray-200 disabled:text-foreground/80"
       )}
       {...props}
     >
