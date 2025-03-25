0605595dbc067cff718d3c1d99846677e3109bf5
Sarah Pickrahn <spickrahn@gmail.com>
2025-03-24 17:01:04 -0700

    use sencondary font color for footer

--- src/app/components/designSystem/Footer.tsx
+++ src/app/components/designSystem/Footer.tsx
@@ -2,7 +2,7 @@ export type FooterProps = {
   label: string;
 };
 export const Footer = ({ label }: FooterProps) => (
-  <footer className="fixed w-full bottom-0 flex justify-center items-center text-center p-4 bg-inherit drop-shadow-[0_-1px_1px_rgba(0,0,0,0.05)]">
+  <footer className="fixed w-full bottom-0 flex justify-center items-center text-center p-4 text-foreground-secondary bg-inherit drop-shadow-[0_-1px_1px_rgba(0,0,0,0.05)]">
     <p>{label}</p>
   </footer>
 );
